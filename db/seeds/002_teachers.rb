2bc5ad41c3f5c27216e7705af7f61eef8ad59493
Marta Wyszynska <marwyszynska@gmail.com>
2016-09-03 19:09:25 +0200

    update teachers seed

--- db/seeds/002_teachers.rb
+++ db/seeds/002_teachers.rb
@@ -1,12 +1,19 @@
-require_relative 'db/samples/name_generator'
+require_relative '../samples/name_generator'
 
-teachers = 10.times do
+10.times do
   name = NameGenerator.new.generate
   Teacher.where(first_name: name.first, last_name: name.last).first_or_create! do |teacher|
     teacher.email = name.map(&:parameterize).join('.') + '@example.com'
   end
 end
 
-teachers.each do |teacher|
-  2.times { teacher.fields << Field.all.sample }
+Teacher.all.each do |teacher|
+  amount = (2..6).to_a.sample
+
+  amount.times do
+    if teacher.fields.size < amount
+      field = Field.all.sample
+      teacher.fields << field unless teacher.fields.include?(field)
+    end
+  end
 end
