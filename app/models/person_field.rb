32f4da66934702228a914befa3ba13fca5b021f6
Marta Wyszynska <marwyszynska@gmail.com>
2016-09-07 17:11:11 +0200

    fix the field choosing for users
    there was a validation issue in case both teacher and user had the same
    id and were about to choose the same field

--- app/models/person_field.rb
+++ app/models/person_field.rb
@@ -2,6 +2,6 @@ class PersonField < ActiveRecord::Base
   belongs_to :fieldable, polymorphic: true
   belongs_to :field
 
-  validates :field, uniqueness: { scope: :fieldable }
+  validates :field, uniqueness: { scope: [:fieldable_type, :fieldable_id] }
   validates :field, presence: true
 end
