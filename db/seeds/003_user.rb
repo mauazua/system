2939b7460440891d9156f19869fd58d01bcba2a6
Marta Wyszynska <marwyszynska@gmail.com>
2016-09-17 19:52:28 +0200

    add seed with users

--- db/seeds/003_user.rb
+++ db/seeds/003_user.rb
@@ -1,1 +1,23 @@
+require_relative '../samples/name_generator'
+
 User.where(email: 'marwyszynska@gmail.com').first_or_create { |u| u.password = 'testowe123' }
+
+50.times do
+  name = NameGenerator.new.generate
+  User.where(first_name: name.first, last_name: name.last).first_or_create! do |user|
+    user.password = 'randomrandom'
+    user.password_confirmation = 'randomrandom'
+    user.email = name.map(&:parameterize).join('.') + '@example.com'
+  end
+end
+
+User.all.each do |user|
+  amount = (1..3).to_a.sample
+
+  amount.times do
+    if user.fields.size < amount
+      field = Field.all.sample
+      user.fields << field unless user.fields.include?(field)
+    end
+  end
+end
